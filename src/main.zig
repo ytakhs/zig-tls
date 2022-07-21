496aa486d54eab199dae19961ec4619c4fa4b241
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-07-21 21:17:33 +0900

    Use snake case

--- src/main.zig
+++ src/main.zig
@@ -5,16 +5,19 @@ const HandshakeType = enum(u8) {
     client_hello = 1,
 };
 
-const ClientHelloHandshake = struct { handshakeType: HandshakeType, length: u24 };
+const ClientHelloHandshake = struct {
+    handshake_type: HandshakeType,
+    length: u24,
+};
 
 const ClientHello = struct {
     const Self = @This();
 
     pub fn decode(raw: []const u8) ClientHelloHandshake {
-        const handshakeType = @intToEnum(HandshakeType, raw[0]);
+        const handshake_type = @intToEnum(HandshakeType, raw[0]);
         const length = std.mem.readIntSliceBig(u24, raw[1..4]);
 
-        return .{ .handshakeType = handshakeType, .length = length };
+        return .{ .handshake_type = handshake_type, .length = length };
     }
 
     pub const Encoder = struct {
@@ -30,7 +33,7 @@ const ClientHello = struct {
         }
 
         pub fn encode(self: *Encoder) ![]const u8 {
-            try self.buf.append(@enumToInt(self.handshake.handshakeType));
+            try self.buf.append(@enumToInt(self.handshake.handshake_type));
 
             var lengthBuf: [3]u8 = undefined;
             std.mem.writeIntSlice(u24, &lengthBuf, self.handshake.length, .Big);
@@ -45,7 +48,7 @@ test "ClientHello" {
     const data: []const u8 = &[_]u8{ @enumToInt(HandshakeType.client_hello), 0x10, 0x00, 0x00 };
     const handshake = ClientHello.decode(data);
 
-    try testing.expectEqual(handshake.handshakeType, .client_hello);
+    try testing.expectEqual(handshake.handshake_type, .client_hello);
     try testing.expectEqual(handshake.length, 1048576);
 
     var encoder = ClientHello.Encoder.init(testing.allocator, handshake);
