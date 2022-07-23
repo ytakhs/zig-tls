68b1278d89dccb342c3bef78940a7ab9c9fe49a1
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-07-23 15:17:04 +0900

    cipher_suites

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,8 @@ const ClientHelloHandshake = struct {
     random: u32,
     session_id_length: u8,
     session_id: []const u8,
+    cipher_suites_length: u16,
+    cipher_suites: []const u8,
 };
 
 const ClientHello = struct {
@@ -35,6 +37,8 @@ const ClientHello = struct {
             const random = self.readIntBig(u32, 4);
             const session_id_length = self.readIntBig(u8, 1);
             const session_id = self.readSlice(session_id_length);
+            const cipher_suites_length = self.readIntBig(u16, 2);
+            const cipher_suites = self.readSlice(cipher_suites_length);
 
             return .{
                 .handshake_type = handshake_type,
@@ -43,6 +47,8 @@ const ClientHello = struct {
                 .random = random,
                 .session_id_length = session_id_length,
                 .session_id = session_id,
+                .cipher_suites_length = cipher_suites_length,
+                .cipher_suites = cipher_suites,
             };
         }
 
@@ -79,23 +85,28 @@ const ClientHello = struct {
         }
 
         pub fn encode(self: *Encoder) ![]const u8 {
+            // handshake_type
             try self.buf.append(@enumToInt(self.handshake.handshake_type));
-
+            // length
             var length_buf: [3]u8 = undefined;
             std.mem.writeIntSlice(u24, &length_buf, self.handshake.length, .Big);
             try self.buf.appendSlice(length_buf[0..]);
-
+            // protocol_version
             var protocol_version_buf: [2]u8 = undefined;
             std.mem.writeIntSlice(u16, &protocol_version_buf, self.handshake.protocol_version, .Big);
             try self.buf.appendSlice(protocol_version_buf[0..]);
-
+            // random
             var random_buf: [4]u8 = undefined;
             std.mem.writeIntSlice(u32, &random_buf, self.handshake.random, .Big);
             try self.buf.appendSlice(random_buf[0..]);
-
+            // session_id
             try self.buf.append(self.handshake.session_id_length);
-
             try self.buf.appendSlice(self.handshake.session_id[0..self.handshake.session_id_length]);
+            // cipher_suites
+            var cipher_suites_length_buf: [2]u8 = undefined;
+            std.mem.writeIntSlice(u16, &cipher_suites_length_buf, self.handshake.cipher_suites_length, .Big);
+            try self.buf.appendSlice(cipher_suites_length_buf[0..]);
+            try self.buf.appendSlice(self.handshake.cipher_suites[0..self.handshake.cipher_suites_length]);
 
             return self.buf.items;
         }
@@ -107,31 +118,33 @@ test "ClientHello" {
         @enumToInt(HandshakeType.client_hello),
     };
     const length = &[_]u8{
-        0x10,
-        0x00,
-        0x00,
+        0x10, 0x00, 0x00,
     };
     const protocol_version = &[_]u8{
-        0x03,
-        0x03,
+        0x03, 0x03,
     };
     const random = &[_]u8{
-        0x00,
-        0x00,
-        0x00,
-        0x00,
+        0x00, 0x00, 0x00, 0x00,
     };
     const session_id_length = &[_]u8{
         0x20,
     };
     const sesion_id = &[_]u8{0x00} ** 0x20;
+    const cipher_suites_length = &[_]u8{
+        0x00, 0x02,
+    };
+    const cipher_suites = &[_]u8{
+        0x00, 0x9c,
+    };
     const raw =
         handshake_type ++
         length ++
         protocol_version ++
         random ++
         session_id_length ++
-        sesion_id;
+        sesion_id ++
+        cipher_suites_length ++
+        cipher_suites;
 
     var decoder = ClientHello.Decoder.init(raw);
     const handshake = decoder.decode();
@@ -142,6 +155,8 @@ test "ClientHello" {
     try testing.expectEqual(handshake.random, 0x00);
     try testing.expectEqual(handshake.session_id_length, 32);
     try testing.expectEqualSlices(u8, handshake.session_id, &[_]u8{0x00} ** 32);
+    try testing.expectEqual(handshake.cipher_suites_length, 2);
+    try testing.expectEqualSlices(u8, handshake.cipher_suites, &[_]u8{ 0x00, 0x9c });
 
     var encoder = ClientHello.Encoder.init(testing.allocator, handshake);
     defer encoder.deinit();
