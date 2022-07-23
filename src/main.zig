8cf9bc2183bab696b000248d83149357f0fcc90f
ytakhs <21127627+ytakhs@users.noreply.github.com>
2022-07-23 23:48:09 +0900

    All your codebase are belongs to us.

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,12 @@
+const std = @import("std");
 const client_hello = @import("./client_hello.zig");
 
-test {
+pub fn main() anyerror!void {
+    // Note that info level log messages are by default printed only in Debug
+    // and ReleaseSafe build modes.
+    std.log.info("All your codebase are belong to us.", .{});
+}
+
+test "" {
     _ = client_hello;
 }
